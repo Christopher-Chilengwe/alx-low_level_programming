3fdd32ced79379926f9e7ecd84221175f5441b07
Christopher Chilengwe <christopherchilengwe@gmail.com>
2023-08-07 16:41:02 +0200

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,52 +1,52 @@
-#include "main.h" 
-#include <stdlib.h> 
-#include <stdio.h> 
-  
- /** 
-  * _strlen - return length of a string 
-  * 
-  * @s: char type 
-  * Return:  length of string 
-  */ 
- int _strlen(char *s) 
- { 
-         int a; 
-  
-         for (a = 0; s[a] != '\0'; a++) 
-         { 
-  
-         } 
-         return (a); 
- } 
-  
- /** 
-  * *_strdup - function to return pointer to string 
-  * @str: pointer to string array input 
-  * Return: pointer to string created 
-  */ 
-  
- char *_strdup(char *str) 
- { 
-         char *ptr; 
-         int size; 
-         int x; 
-  
-         if (str == NULL) 
-         { 
-                 return (NULL); 
-         } 
-         size = _strlen(str) + 1; 
-         ptr = malloc(size * sizeof(char)); 
-  
-         x = 0; 
-         while (x < size) 
-         { 
-                 if (ptr == NULL) 
-                 { 
-                         return (NULL); 
-         } 
-                 ptr[x] = str[x]; 
-                 x++; 
-         } 
-         return (ptr); 
- }
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: character pointer
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+    int length;
+
+    for (length = 0; s[length] != '\0'; length++)
+    {
+
+    }
+    return (length);
+}
+
+/**
+ * *_strdup - function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter
+ * @str: pointer to string array input
+ * Return: pointer to the newly copied string
+ */
+
+char *_strdup(char *str)
+{
+    char *ptr;
+    int size;
+    int i;
+
+    if (str == NULL)
+    {
+        return (NULL);
+    }
+    size = _strlen(str) + 1;
+    ptr = malloc(size * sizeof(char));
+
+    i = 0;
+    while (i < size)
+    {
+        if (ptr == NULL)
+        {
+            return (NULL);
+        }
+        ptr[i] = str[i];
+        i++;
+    }
+    return (ptr);
+}
