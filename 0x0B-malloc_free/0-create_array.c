22019c12c36877ab0f4194add45370674bf038ff
Christopher Chilengwe <christopherchilengwe@gmail.com>
2023-08-07 18:56:35 +0200

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,45 +1,24 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * str_concat - Concatenates two strings.
- * @s1: First string.
- * @s2: Second string.
+ * create_array - create array of size size and assign char c
+ * @size: size of array
+ * @c: char to assign
  *
- * Return: Pointer to newly allocated space containing concatenated strings,
- *         NULL if allocation fails.
+ * Return: pointer to array, NULL if fail
  */
-char *str_concat(char *s1, char *s2)
+char *create_array(unsigned int size, char c)
 {
-    char *concat;
-    unsigned int len1, len2, i, j;
+    char *str;
+    unsigned int i;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+    str = malloc(sizeof(char) * size);
 
-    len1 = 0;
-    while (s1[len1] != '\0')
-        len1++;
-
-    len2 = 0;
-    while (s2[len2] != '\0')
-        len2++;
-
-    concat = malloc(sizeof(char) * (len1 + len2 + 1));
-
-    if (concat == NULL)
+    if (size == 0 || str == NULL)
         return (NULL);
 
-    for (i = 0; i < len1; i++)
-        concat[i] = s1[i];
+    for (i = 0; i < size; i++)
+        str[i] = c;
 
-    for (j = 0; j < len2; j++)
-        concat[i + j] = s2[j];
-
-    concat[i + j] = '\0';
-
-    return (concat);
+    return (str);
 }
